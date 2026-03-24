26d0a280689e8116e9c11b0d3505917703dd4918
Aljes <aleksandr.binkevich@gmail.com>
2026-03-24 14:03:00 +0000

    Add script to download PBMC 10K dataset

---
+++ scripts/download_data.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mkdir -p data
+wget -O data/pbmc10k.h5 https://cf.10xgenomics.com/samples/cell-exp/4.0.0/SC3_v3_NextGem_SI_PBMC_10K/SC3_v3_NextGem_SI_PBMC_10K_filtered_feature_bc_matrix.h5
